453ae5f41d48f6f80751a8e20eef65fae194d02a
Max <zenit-max@example.com>
2022-11-16 12:56:36 +0300

    Create Readme.txt

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿//Написать программу, которая из имеющенгося массива строк формирует массив строк,
+//Длинна которого меньше либо равна 3.
+
+System.Console.Write("Введите длину массива");
+int len = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите строку");
+string [] str = new string[len];
+for (int i = 0; i < len; i++)
+{
+    str[i] = Console.ReadLine();
+}
+System.Console.WriteLine("Введенный массив строк");
+System.Console.Write("[ ");
+for (int i = 0; i < len; i++)
+{
+    System.Console.Write(str[i] + ", ");
+}
+System.Console.WriteLine(" ]");
+System.Console.WriteLine("Отсортированный массив ");
+System.Console.Write("[ ");
+for (int i = 0; i < len; i++)
+{
+    if(str[i].Length <=3) System.Console.Write(str[i]+ ", ");
+}
+System.Console.WriteLine(" ]");
+
